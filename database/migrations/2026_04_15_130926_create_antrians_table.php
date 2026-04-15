f7a280aebbd9363a74de29ecb7be832f69762a2d
BachtiarJA <bachtiarjulianandi@gmail.com>
2026-04-15 20:43:28 +0700

    migrate database

---
+++ database/migrations/2026_04_15_130926_create_antrians_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('antrians', function (Blueprint $table) {
+            $table->id();
+
+            // Perhatikan constrained() di bawah ini sudah disesuaikan dengan nama tabel Anda
+            $table->foreignId('pasien_id')->constrained('pasiens')->onDelete('cascade');
+            $table->foreignId('dokter_id')->constrained('dokter')->onDelete('cascade'); // <-- merujuk ke tabel 'dokter'
+
+            $table->date('tgl_kunjungan');
+            $table->integer('no_antrian');
+            $table->enum('status', ['menunggu', 'diperiksa', 'selesai', 'batal'])->default('menunggu');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('antrians');
+    }
+};
---
+++ database/migrations/2026_04_15_131619_create_resep_obat_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resep_obats', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('pemeriksaan_id')->constrained('pemeriksaans')->onDelete('cascade');
+
+            $table->string('nama_obat');
+            $table->string('dosis');
+            $table->string('aturan');
+            $table->integer('berapa_kali');
+
+            $table->json('waktu');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resep_obat');
+    }
+};
